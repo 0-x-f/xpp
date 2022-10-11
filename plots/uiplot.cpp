2e4c4b47e88d50f475a5fd53dce17472925e1faa
amaretur <dmitry.rannev24work@gmail.com>
2022-10-11 14:06:06 +0300

    Update UIPlot
    Add DrawPlot function
    Refactor

--- plots/uiplot.cpp
+++ plots/uiplot.cpp
@@ -4,8 +4,9 @@
 #include <cmath>
 #include <limits>
 #include <vector>
-#include<algorithm>
-#include "plot.h"
+#include <algorithm>
+
+#include "uiplot.h"
 #include "../xconfig.h"
 
 UIPlot::UIPlot(XConfig& plot) : IPlot(plot) {
@@ -13,31 +14,31 @@ UIPlot::UIPlot(XConfig& plot) : IPlot(plot) {
 	this->mMaxY = std::numeric_limits<double>::quiet_NaN();
 	this->mMinY = std::numeric_limits<double>::quiet_NaN();
 	
-	Display* display = this->mConfig.display;
-
 	XGCValues gcv;
 	gcv.background = 0xFFFFFF;
 	gcv.foreground = 0x000000;
 
 	this->mContext = XCreateGC(
-		display,
+		this->mConfig.display,
 		this->mWindow,
 		GCForeground | GCBackground,
 		&gcv
 	);
+
+	this->padding = 10;
 }
 
-void UIPlot::Calc() {
+void UIPlot::CalculateFunction() {
 
 	this->mValues = std::vector<double>(this->mPlotIn.Count(), 0);
 
-	for(int i = 0; i<this->mPlotIn.Count(); i++) {
+	for(int i = 0; i < this->mPlotIn.Count(); i++) {
 		
 		// calculate the value
 		this->mValues[i] = this->mPlotFunc(
-			this->mPlotIn.start + this->mPlotIn.step * i
+				this->mPlotIn.GetX(i)
 		);
-		
+	    
 		// serch max value
 		if (std::isnan(this->mMaxY) || this->mMaxY < this->mValues[i]) 
 			this->mMaxY = this->mValues[i];
@@ -48,4 +49,103 @@ void UIPlot::Calc() {
 	};
 }
 
+inline double UIPlot::CalculateUnitSegment(
+	double a, 
+	double b, 
+	unsigned int length
+) {
+	
+	if ((a < 0) ^ (b < 0)) {
+		return 1.0 * (length - this->padding*2) / (abs(a) + abs(b));
+	} else {
+		return 1.0 * (length - this->padding*2) / std::max(abs(a), abs(b));
+	}
+}
+
+void UIPlot::CalculateGeometry() {
+
+	// calculate unit segment
+	double unitSegY = this->CalculateUnitSegment(
+		this->mMaxY, 
+		this->mMinY, 
+		this->mConfig.height
+	);
+	
+	double unitSegX = this->CalculateUnitSegment(
+		this->mPlotIn.start, 
+		this->mPlotIn.stop, 
+		this->mConfig.width
+	);
+	
+	this->mUnitSeg = std::min(unitSegY, unitSegX);
+	
+	// calculate coordinates center
+	this->mCenter.y = this->mMaxY <= 0 ? 0 : round(unitSegY * this->mMaxY);
+
+	double minX = std::min(this->mPlotIn.start, this->mPlotIn.stop);
+	this->mCenter.x = minX >= 0 ? 0 : round(unitSegX * abs(minX)); 
+
+	// add padding to plot
+	this->mCenter.y += this->padding;
+	this->mCenter.x += this->padding;
+}
+
+void UIPlot::DrawLine(const UIPoint& a, const UIPoint& b) {
+	XDrawLine(
+		this->mConfig.display,
+		this->mWindow,
+		this->mContext,
+		a.x, a.y,
+		b.x, b.y
+	);
+}
+
+void UIPlot::DrawPlot() {
+
+	if (this->mPlotFunc == nullptr)
+		return;
+
+	// calculation of function values in mPlotIn
+	this->CalculateFunction(); 
+
+	// caclulation plot geometry (unit segment, coordinates center)
+	this->CalculateGeometry();
+
+	// draw OX
+	UIPoint a = UIPoint(0, this->mCenter.y);
+	UIPoint b = UIPoint(this->mConfig.width, this->mCenter.y); 
+    
+	this->DrawLine(a, b);    
+    
+	// draw OY    
+	a = UIPoint(this->mCenter.x, 0);
+	b = UIPoint(this->mCenter.x, this->mConfig.height);
+	
+	this->DrawLine(a, b);
+
+	// draw plot
+	UIPoint pPoint;
+	bool pPointIsNan = true;
+	
+	for (int i = 0; i < this->mValues.size(); i++) {
+		
+		if (std::isnan(this->mValues[i]) || std::isinf(this->mValues[i])) {
+			pPointIsNan = true;
+			continue;
+		}
+
+		UIPoint cPoint = UIPoint(
+			this->mCenter.x + this->mPlotIn.GetX(i) * this->mUnitSeg, 
+			this->mCenter.y - (this->mValues[i] * this->mUnitSeg)
+		);
+
+		if (pPointIsNan) {
+			pPointIsNan = false;
+		} else {
+			this->DrawLine(pPoint, cPoint);
+		}
+
+		pPoint = cPoint;
+	}
+}
 
--- plots/uiplot.h
+++ plots/uiplot.h
@@ -9,6 +9,21 @@
 #include "../iplot.h"
 #include "../xconfig.h"
 
+struct UIPoint {
+	unsigned int x;
+	unsigned int y;
+
+	UIPoint() {
+		x = 0;
+		y = 0;
+	}
+
+	UIPoint(unsigned int xp, unsigned int yp) {
+		x = xp;
+		y = yp;
+	}
+};
+
 class UIPlot :
 	public IPlot {
 
@@ -17,12 +32,24 @@ class UIPlot :
 	public:
 		virtual void DrawPlot() override;
 	private:
-		void Calc();
+		void CalculateFunction();
+		void CalculateGeometry();
+		inline double CalculateUnitSegment(
+			double a, 
+			double b, 
+			unsigned int length
+		);
+	private:
+		void DrawLine(const UIPoint& a, const UIPoint& b);
+	private:
+		unsigned short int padding;
 	protected:
 		std::vector<double> mValues;
 	protected:
 		double mMaxY;
 		double mMinY;
+	protected:
+		UIPoint mCenter;
 	protected:
 		unsigned mUnitSeg;
 	protected:
