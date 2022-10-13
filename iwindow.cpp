22a444c0e99da77fb8ca3cea89082f1ba189b3e2
amaretur <dmitry.rannev24work@gmail.com>
2022-10-13 18:23:24 +0300

    Add ClassList and support for child elements

--- iwindow.cpp
+++ iwindow.cpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <vector>
+
 #include "iwindow.h"
 
 IWindow::IWindow(XConfig& window) {
@@ -51,4 +54,95 @@ bool IWindow::IsOwner(XEvent& event) const {
 	return event.xany.window == this->GetWindow();
 }
 
+void IWindow::AddChild(IWindow& child) {
+	this->mChildItems.push_back(&child);
+}
+
+void IWindow::RemoveChild(const std::string& className) {
+
+	unsigned int iSize = this->mChildItems.size();
+
+	for(unsigned int i = 0; i < iSize; i++) {
+
+		if (this->mChildItems[i]->ClassList.Contains(className) != true) 
+			continue;
+
+		// if it is not the last item
+		if (i != iSize-1) 
+			this->mChildItems[i] = this->mChildItems[iSize];
+
+		this->mChildItems.pop_back();
+		iSize--;
+	}
+}
+
+IWindow* IWindow::FindChild(const std::string& className) const {
+
+	for(unsigned int i = 0; i < this->mChildItems.size(); i++) {
+		if (this->mChildItems[i]->ClassList.Contains(className)) 
+			return this->mChildItems[i];
+	}
+
+	return nullptr;
+}
+
+std::vector<IWindow*> IWindow::FindChildAll(
+	const std::string& className
+) const {
+
+	std::vector<IWindow*> items;
+
+	for(unsigned int i = 0; i < this->mChildItems.size(); i++) {
+		if (this->mChildItems[i]->ClassList.Contains(className)) 
+			items.push_back(this->mChildItems[i]);
+	}
+
+	return items;
+}
+
+void UIClassList::Add(const std::string& className) {
+	if (!(this->Contains(className))) 
+		this->mClassList.push_back(className);
+}
+
+void UIClassList::Remove(const std::string& className) {
+
+	unsigned int iSize = this->mClassList.size();
+
+	for(unsigned int i = 0; i < iSize; i++) {
+
+		if (this->mClassList[i] != className) 
+			continue;
+
+		// if it is not the last item
+		if (i != iSize-1) 
+			this->mClassList[i] = this->mClassList[iSize];
+
+		this->mClassList.pop_back();
+
+		return;
+	}
+}
+
+bool UIClassList::Contains(const std::string& className) const {
+
+	for(unsigned int i = 0; i < this->mClassList.size(); i++) {
+		if (this->mClassList[i] == className) 
+			return true;
+	}
+
+	return false;
+}
+
+void UIClassList::Toggle(const std::string& className) {
+
+	unsigned int size = this->mClassList.size();
+
+	this->Remove(className);
+
+	// if the size has not changed -> removal has not occurred -> 
+	// item was not -> need to be added
+	if (size == this->mClassList.size()) 
+		this->mClassList.push_back(className);
+}
 
--- iwindow.h
+++ iwindow.h
@@ -4,8 +4,20 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <functional>
+#include <string>
+#include <vector>
 
 #include "xconfig.h"
+
+class UIClassList {
+	private:
+		std::vector<std::string> mClassList;
+	public:
+		void Add(const std::string& className);
+		void Remove(const std::string& className);
+		bool Contains(const std::string& className) const;
+		void Toggle(const std::string& className);
+}; 
 
 class IWindow {
 	public:
@@ -23,6 +35,18 @@ class IWindow {
 		virtual unsigned int GetHeight() const;
 	public:
 		virtual bool IsOwner(XEvent& event) const;
+	public:
+		virtual void AddChild(IWindow& child);
+		virtual void RemoveChild(const std::string& className);
+	public:
+		virtual IWindow* FindChild(const std::string& className) const;
+		virtual std::vector<IWindow*> FindChildAll(
+			const std::string& className
+		) const;
+	public:
+		UIClassList ClassList;
+	protected:
+		std::vector<IWindow*> mChildItems;
 	protected:
 		Window mWindow;
 		XConfig mConfig;
